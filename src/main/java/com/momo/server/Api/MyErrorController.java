f71605e2c354229c767dfc884db6a7b9bb2d25b3
suram <swamys@naver.com>
2021-05-10 02:11:11 +0900

    chore : 에러 컨트롤러 비활성화

--- src/main/java/com/momo/server/Api/MyErrorController.java
+++ src/main/java/com/momo/server/Api/MyErrorController.java
@@ -4,7 +4,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+//@Controller
 public class MyErrorController implements ErrorController {
 	
 	@RequestMapping("/error")
