5d0c0f5351a0a861f666236a521603aeb9253609
suram <swamys@naver.com>
2021-07-03 20:14:29 +0900

    fix : 사용자 생성시 userTimes 초기화 rows 개수 수정

--- src/main/java/com/momo/server/dto/request/LoginRequestDto.java
+++ src/main/java/com/momo/server/dto/request/LoginRequestDto.java
@@ -1,11 +1,14 @@
 package com.momo.server.dto.request;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class LoginRequestDto {
 
     private String username;
+    @ApiModelProperty(example = "75d57c19141e6fd")
     private String meetId;
     private Boolean remember;
 }
