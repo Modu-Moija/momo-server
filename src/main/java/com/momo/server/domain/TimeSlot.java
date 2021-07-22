c7cb2ea0521b5ef5f7d8b02449ecb099e7a871d9
choeunhak <02069@naver.com>
2021-07-22 19:13:06 +0900

    chore: test commit

--- src/main/java/com/momo/server/domain/TimeSlot.java
+++ src/main/java/com/momo/server/domain/TimeSlot.java
@@ -6,11 +6,9 @@ import java.util.ArrayList;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
