f713f2e36d07952a6ebd1d739439329622026c47
choeunhak <02069@naver.com>
2021-07-22 23:04:58 +0900

    fix: 로그인 로직 수정

--- src/main/java/com/momo/server/controller/UserController.java
+++ src/main/java/com/momo/server/controller/UserController.java
@@ -42,31 +42,24 @@ public class UserController {
 	User userEntity = userService.login(loginRequestDto);// 추후에 로직 수정해야함
 	ResponseEntity<?> responseCode;
 
-	if (userEntity == null) {// 유저 존재하지않음(신규유저)
-	    userEntity = userService.createUser(loginRequestDto);
-	    // 세션에 정보저장, 일단 세션으로 구현해놨습니다.. 추후에 쿠키로 수정할수있습니다.
-	    HttpSession session = request.getSession();
-	    session.setAttribute("user", userEntity);
+	HttpSession session = request.getSession();
+	session.setAttribute("user", loginRequestDto);
 
-	    request.setAttribute("authuser", userEntity);
-	    Aes128 aes128 = new Aes128(key);
-	    String enc = aes128.encrypt(userEntity.getUserId().toString());
-	    Cookie authCookie = new Cookie("authuser", enc);
-	    response.addCookie(authCookie);
+	request.setAttribute("authuser", loginRequestDto);
+	Aes128 aes128 = new Aes128(key);
+	String enc = aes128.encrypt(loginRequestDto.getUsername().toString());// 유저이름으로 암호화시켜도 안전한지 모르겠습니다
+	System.out.println(enc);
+	Cookie authCookie = new Cookie("authuser", enc);
+	response.addCookie(authCookie);
+
+	// 유저 존재하지않음(신규유저)
+	if (userEntity == null) {
+	    userEntity = userService.createUser(loginRequestDto);
 
 	    responseCode = ResponseEntity.status(HttpStatus.CREATED).build();
 	    return new ResponseEntity<>(new CmRespDto<>(responseCode, "신규 유저 로그인 성공", null), HttpStatus.CREATED);
 
 	} else {// 유저 존재(기존 유저)
-
-	    HttpSession session = request.getSession();
-	    session.setAttribute("user", userEntity);
-
-	    request.setAttribute("authuser", userEntity);
-	    Aes128 aes128 = new Aes128(key);
-	    String enc = aes128.encrypt(userEntity.getUserId().toString());
-	    Cookie authCookie = new Cookie("authuser", enc);
-	    response.addCookie(authCookie);
 
 	    responseCode = ResponseEntity.status(HttpStatus.OK).build();
 	    return new ResponseEntity<>(new CmRespDto<>(responseCode, "기존 유저 로그인 성공", null), HttpStatus.OK);
