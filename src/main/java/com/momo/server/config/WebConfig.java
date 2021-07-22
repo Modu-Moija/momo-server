afd2a382cc9e7372c0fcb311ac14489b63632e5f
choeunhak <02069@naver.com>
2021-07-22 21:11:49 +0900

    fix: cor에러 추가하기 위해 enablewebmvc 어노테이션 적용

--- src/main/java/com/momo/server/config/WebConfig.java
+++ src/main/java/com/momo/server/config/WebConfig.java
@@ -2,18 +2,16 @@ package com.momo.server.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE");
+	registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
-
 
 }
