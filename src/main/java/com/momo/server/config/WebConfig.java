aa230727354f6c2b53783b6878093a9cbb775431
choeunhak <02069@naver.com>
2021-07-22 22:11:46 +0900

    fix: cors에러 수정

--- src/main/java/com/momo/server/config/WebConfig.java
+++ src/main/java/com/momo/server/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-	registry.addMapping("/**").allowedOrigins("https://momoapi.azurewebsites.net/").allowedMethods("GET", "POST",
-		"PUT", "DELETE");
+	registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT",
+		"DELETE");
     }
 
 }
