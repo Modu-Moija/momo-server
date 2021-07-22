98d301f00c27fda59588a13429666a42de1493a1
choeunhak <02069@naver.com>
2021-07-22 22:27:39 +0900

    fix: cors에러 credentials true로 수정

--- src/main/java/com/momo/server/config/WebConfig.java
+++ src/main/java/com/momo/server/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-	registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT",
-		"DELETE");
+	registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true).allowedMethods("GET",
+		"POST", "PUT", "DELETE");
     }
 
 }
