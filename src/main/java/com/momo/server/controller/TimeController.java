24a85705b6103fb15e674e0017497f2fb78beea1
suram <swamys@naver.com>
2021-08-08 23:36:02 +0900

    response data test

--- src/main/java/com/momo/server/controller/TimeController.java
+++ src/main/java/com/momo/server/controller/TimeController.java
@@ -58,4 +58,16 @@ public class TimeController {
 	MostLeastRespDto mostLeastRespDto = timeService.getMostLeastTime(meetId);
 	return mostLeastRespDto;
     }
+
+    @GetMapping("/2d")
+    public ResponseEntity<?> get2DTimetable(){
+        int[][] data = {{2, 0, 0, 0}, {2,0,0,0}, {0,1,0,1}, {0,2,2,1}, {2,0,0,1}, {2,1,1,0},{2,1,1,3}};
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("/array")
+    public ResponseEntity<?> getArrayTimetable(){
+        int[] data = {128, 128, 17, 41, 129, 148, 151};
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
 }
