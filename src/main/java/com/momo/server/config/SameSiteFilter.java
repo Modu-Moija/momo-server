0d9e77c38fb1c390cd26001a467b5ed19b7ff64d
choeunhak <02069@naver.com>
2021-09-21 07:14:00 +0900

    fix: 무조건 응답에 추가시키기

--- src/main/java/com/momo/server/config/SameSiteFilter.java
+++ src/main/java/com/momo/server/config/SameSiteFilter.java
@@ -11,18 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SameSiteFilter implements Filter {
 
-    private static final String THIRD_PARTY_URI = "/third/party/uri";
-
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        if(THIRD_PARTY_URI.equals(request.getRequestURI())) {
-            chain.doFilter(request, new CustomHttpServletResponseWrapper(response));
-        } else {
-            chain.doFilter(request, response);
-        }
+        chain.doFilter(request, new CustomHttpServletResponseWrapper(response));
     }
 
 }
